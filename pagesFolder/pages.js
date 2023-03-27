f0ddbc2d9b59e7d3758b6c2ec6dbb590f2e992dc
Adedayo-Onabamiro <dayoonabamiro@gmail.com>
2023-03-27 14:36:06 +0100

    modal Fully Functional

--- pagesFolder/pages.js
+++ pagesFolder/pages.js
@@ -38,15 +38,20 @@ searchInputBtn.addEventListener('click', ()=>{
   var DBMealName = document.getElementById("DBMealName");
   var DBCategory = document.getElementById("DBCategory");
   var instructionsText = document.getElementById("instructionsText");
+  var ingredientContentBox = document.getElementById("ingredientContentBox");
 
 
   let api_img_link = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchInputValue}`;
   let galleryBox = document.getElementById('galleryItems');
+  var pillsBox = document.getElementById("pillsBox");
   fetch(api_img_link)
   .then(Response => Response.json())
   .then(result => {
     console.log(result);
     galleryBox.innerHTML = "";
+    pillsBox.innerHTML = "";
+    ingredientContentBox.innerHTML = "";
+
     if (result.meals == null) {
       statusMsg.style.display = "block"
       galleryBox.innerHTML = "";
@@ -61,6 +66,7 @@ searchInputBtn.addEventListener('click', ()=>{
           {
             //code tocreate the gallery items individually
             var galleryItem = document.createElement('div');
+            galleryItem.setAttribute("onclick", `details("${meal.idMeal}")`)
             galleryItem.className = "galleryItem";
             var galleryContent = 
             `
@@ -69,36 +75,7 @@ searchInputBtn.addEventListener('click', ()=>{
             `;
             galleryItem.innerHTML = galleryContent;
             galleryBox.appendChild(galleryItem);
-            //code tocreate the gallery items individually ends
-
-            //code for modal section
-            var GIL = result.meals.length; 
-            console.log(`there are gallery items : ${GIL}`);
-            for (let x = 0; x < GIL; x++) {
-              galleryItem.addEventListener("click", ()=>{
-                var modal = document.getElementById("myModalBox");
-                var span = document.getElementsByClassName("close")[0];
-                span.onclick = function() {
-                  modal.style.display = "none";
-                }
-                // When the user clicks anywhere outside of the modal, close it
-                window.onclick = function(event) {
-                  if (event.target == modal) {
-                    modal.style.display = "none";
-                  }
-                }
-                modal.style.display = "block"; //display the modal
-                  console.log("modal open");
-                  // galleryBox.innerHTML = ""; 
-                      // statusMsg.style.display = "none"
-                      // console.log(result.meals.length)
-                          detailsBox1Img.src = `${meal.strMealThumb}`;
-                          DBMealName.innerText = `${meal.strMeal}`;
-                          DBCategory.innerText = `Category: ${meal.strCategory}`;
-                          instructionsText.innerText = `${meal.strInstructions}`;
-              }); 
-            }
-             //code for modal ends
+            //code to create the gallery items individually ends
         }
         });
       } 
@@ -115,41 +92,122 @@ document.getElementById('searchInput').addEventListener("keypress", (event) => {
   }
 });
 
-
-
-
 //Code to display foods in different categories on button click
-let fcb = document.getElementById("FCB");
-for (let x = 0 ; x < FCB.length ; x ++) {
-  FCB[x].addEventListener('click', ()=>{
-    var chosenBtn = FCB[x];
-    var chosenBtnName = chosenBtn.innerText;
-    function buttonApi() {
-      let api_category_link = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${chosenBtnName}`;
-      let galleryBox = document.getElementById('galleryItems');
-      galleryBox.innerHTML = "";
-      fetch(api_category_link)
-      .then(Response => Response.json())
-      .then(result => {
-        console.log(result);
-            result.meals.forEach(meal => {
-              {
-                var galleryItem = document.createElement('div');
-                galleryItem.className = "galleryItem";
-                var galleryContent = 
-                `
-                    <a target="_blank" href="${meal.strMealThumb}">
-                    <img id="galleryImg" class="galleryImg" src="${meal.strMealThumb}">
-                    </a>
-                    <div class="gallerydesc">${meal.strMeal}</div>
-                `;
-                galleryItem.innerHTML = galleryContent;
-                galleryBox.appendChild(galleryItem);
-            }
-            });
-      })
-    }
-    buttonApi();
-  });
+function foodCategory(){
+  let fcb = document.getElementById("FCB");
+  for (let x = 0 ; x < FCB.length ; x ++) {
+    FCB[x].addEventListener('click', ()=>{
+      var chosenBtn = FCB[x];
+      var chosenBtnName = chosenBtn.innerText;
+      function buttonApi() {
+        let api_category_link = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${chosenBtnName}`;
+        let galleryBox = document.getElementById('galleryItems');
+        galleryBox.innerHTML = "";
+        fetch(api_category_link)
+        .then(Response => Response.json())
+        .then(result => {
+          console.log(result);
+              result.meals.forEach(meal => {
+                {
+                  var galleryItem = document.createElement('div');
+                  galleryItem.setAttribute("onclick", `details("${meal.idMeal}")`)
+                  galleryItem.className = "galleryItem";
+                  var galleryContent = 
+                  `
+                      <img id="galleryImg" class="galleryImg" src="${meal.strMealThumb}">
+                      <div class="gallerydesc">${meal.strMeal}</div>
+                  `;
+                  galleryItem.innerHTML = galleryContent;
+                  galleryBox.appendChild(galleryItem);
+              }
+              });
+        })
+      }
+      buttonApi();
+    });
+  }
 }
+foodCategory();
 //Code to display foods in different categories on button click
+
+//code to save meal or remove meal on button click
+
+//code to save meal or remove meal on button click end
+
+
+function details(id) {
+  let api_id_link = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
+  fetch(api_id_link)
+  .then(result => result.json())
+  .then(detail => {
+    var ingredientContentBox = document.getElementById("ingredientContentBox");
+    ingredientContentBox.innerHTML = "";
+    let meal = detail.meals[0]
+            //code for modal section
+            var GIL = detail.meals.length; 
+            // console.log(`there are gallery items : ${GIL}`);
+            for (let x = 0; x < GIL; x++) {
+                var modal = document.getElementById("myModalBox");
+                var span = document.getElementsByClassName("close")[0];
+                span.onclick = function() {
+                  modal.style.display = "none";
+                }
+                // When the user clicks anywhere outside of the modal, close it
+                window.onclick = function(event) {
+                  if (event.target == modal) {
+                    modal.style.display = "none";
+                  }
+                }
+                modal.style.display = "block"; //display the modal
+                  console.log("modal open");
+                  detailsBox1Img.src = `${meal.strMealThumb}`;
+                  DBMealName.innerText = `${meal.strMeal}`;
+                  DBCategory.innerText = `Category: ${meal.strCategory}`;
+                  instructionsText.innerText = `${meal.strInstructions}`;
+                  
+                  //create pills code
+                  pillsBox.innerHTML = "";
+                  var pillContent = `${meal.strTags}`;
+                  var pillContentArr = pillContent.split(",");
+                  console.log(pillContentArr);
+                  for (let x = 0; x < pillContentArr.length; x++) {
+                    if (pillContent == "null") {
+                      console.log("no tags");
+                    } else {
+                      var pill = document.createElement('div');
+                      pill.className = "pill";
+                      pill.innerText = pillContentArr[x];
+                      // console.log(pillContent);
+                      pillsBox.appendChild(pill);  
+                    }
+                  }
+                  //create pills code end
+
+                  //create table for ingredients
+                  let count = 1;
+                  let ingredientArr = [];
+                  for (let i in meal){
+                    let ingredient = "";
+                    let measurement = "";
+                    if (i.startsWith("strIngredient") && meal [i]){
+                      ingredient = meal[i];
+                      measurement = meal[`strMeasure` + count];
+                      count += 1;
+                      ingredientArr.push(ingredient, measurement);
+
+                      let ingredientContentLine = document.createElement("div");
+                      ingredientContentLine.className = "ingredientContentLine"
+                      ingredientContentLineContent = 
+                      `
+                      <p class="ingredient" id="ingredient">${meal[i]}</p>
+                      <p class="measurement" id="measurement">${measurement}</p>
+                      `
+                      ingredientContentLine.innerHTML = ingredientContentLineContent;
+                      document.getElementById("ingredientContentBox").appendChild(ingredientContentLine);
+                    }
+                  }
+                  //create table for ingredients
+            }
+             //code for modal ends
+  }
+)}
